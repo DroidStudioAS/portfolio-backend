61837c6b06e3ab28ccfdd1d6a84d37ebe742f9fd
SSSSDev <aleksandar.smiljanic19@gmail.com>
2024-12-15 13:15:33 +0100

    added mock api call

--- portfoliobackend/wsgi.py
+++ portfoliobackend/wsgi.py
@@ -11,10 +11,11 @@ import os
 import sys
 
 # Add the project directory to the Python path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(current_dir)
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfoliobackend.settings')
 
 application = get_wsgi_application()
